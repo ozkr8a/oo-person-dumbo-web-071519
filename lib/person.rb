ffa607c3d2dbc6f4516d75d6a91c00613e78acdf
Oscar <oscar_ochoac@yahoo.com>
2019-07-17 17:23:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -76,8 +76,8 @@ class Person
       person_arg.happiness += 1
       self.happiness += 1
       
-      check_limits(person_arg.happiness)
-      check_limits(self.happiness)
+      person_arg.happiness = check_limits(person_arg.happiness)
+      self.happiness = check_limits(self.happiness)
       
       return "blah blah sun blah rain"
     else
