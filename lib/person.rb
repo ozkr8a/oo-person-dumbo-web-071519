dbd96323148b6111e0dab5da25f9387f069e1751
Oscar <oscar_ochoac@yahoo.com>
2019-07-17 17:39:53 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -84,8 +84,8 @@ class Person
   
   def start_conversation(person_arg, topic)
     if topic == "politics"
-      person_arg.happiness -= 1
-      self.happiness -= 1
+      person_arg.happiness -= 2
+      self.happiness -= 2
       
       person_arg.happiness = check_limits(person_arg.happiness)
       self.happiness = check_limits(self.happiness)
